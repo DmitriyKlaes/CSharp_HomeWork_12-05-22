3e0c4166bd5cecbf3923a9a4f9382be742885838
DmitriyKlaes <89119584059@mail.ru>
2022-05-16 16:33:20 +0300

    task000 - цетральная цифра из трёхзначного числа

---
+++ task000/Program.cs
@@ -0,0 +1,36 @@
+﻿/*
+Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Пример:
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+//Трехзначное число можно сгенерировать случайно или ввести вручную.
+
+Console.WriteLine("Сгенерировать случайное трёхзначное число? (введите Y или N): ");
+string choice = Console.ReadLine()!;
+if (choice.ToLower() == "y" || choice.ToLower() == "n")
+{
+    int number = 0;
+    if (choice.ToLower() == "y")
+    {
+        number = new Random().Next(100, 1000);
+    }
+    if (choice.ToLower() == "n")
+    {
+        Console.WriteLine("Введите трёхзначное число:");
+        number = Convert.ToInt32(Console.ReadLine());
+        if (number >= 1000)
+        {
+            Console.WriteLine("Неверный ввод! Попробуйте ещё раз.");
+            return;
+        }
+    }
+    int centerNumber = (number % 100) / 10;
+    Console.WriteLine($"Вторая цифра числа {number} это {centerNumber}");
+}
+else
+{
+    Console.WriteLine("Неверный выбор! Попробуйте ещё раз.");
+}
